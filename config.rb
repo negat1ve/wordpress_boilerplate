5c3a06b63c05e8ec4accf485c3ee8a670c9836cf
Ilya <ilya.negat1ve@gmail.com>
2014-02-07 14:52:39 +0400

    Update config.rb

--- config.rb
+++ config.rb
@@ -27,3 +27,56 @@ line_comments = true
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+module Compass::SassExtensions::Functions::Files
+  # Does the supplied image exists?
+  def file_exists(image_file)
+    path = image_file.value
+    # Compute the real path to the image on the file system if the images_dir is set.
+    if Compass.configuration.images_path
+      path = File.join(Compass.configuration.images_path, path)
+    else
+      path = File.join(Compass.configuration.project_path, path)
+    end
+    
+    Sass::Script::Bool.new(File.exists?(path))
+  end
+  
+  # Generate a filename with @2x appended to the end
+  def retina_filename(image_file)
+    filename = image_file.value
+    parts = filename.split('.')
+    ext = parts.pop
+    f = parts.pop
+    f = f + "@2x"
+    parts.push(f)
+    parts.push(ext)
+    Sass::Script::String.new(parts.join('.'))
+  end
+end
+
+module Sass::Script::Functions
+  include Compass::SassExtensions::Functions::Files
+end
+
+
+# Make a copy of sprites with a name that has no uniqueness of the hash.
+on_sprite_saved do |filename|
+  if File.exists?(filename)
+    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+    # Note: Compass outputs both with and without random hash images.
+    # To not keep the one with hash, add: (Thanks to RaphaelDDL for this)
+    FileUtils.rm_rf(filename)
+  end
+end
+ 
+# Replace in stylesheets generated references to sprites
+# by their counterparts without the hash uniqueness.
+on_stylesheet_saved do |filename|
+  if File.exists?(filename)
+    css = File.read filename
+    File.open(filename, 'w+') do |f|
+      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
+    end
+  end
+end
