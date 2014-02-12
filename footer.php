9355c6ca3e7735df1a95730fa4ea93a95e3e6e8e
Ilya <ilya.negat1ve@gmail.com>
2014-02-12 12:33:05 +0400

    New changes

--- footer.php
+++ footer.php
@@ -20,58 +20,12 @@
             </div>
         </div>
         <div class="c-menu-mobile">
-            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'b-nav__list') ); ?>
         </div>
 		<footer class="c-footer">
             <a class="l-credits">Credits</a>
             <nav class="b-nav">
-                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'b-nav__menu' ) ); ?>
-                <!-- <ul class="b-nav__menu">
-                    <li class="b-nav__menu-item">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>about/">About 103</a>
-                        <div class="b-nav__dropdown">
-                            <ul class="b-nav__col">
-                                <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>suite-rewards/">Suite Rewards</a></li>
-                                <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>our-team/">Our Team</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="b-nav__menu-item">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>services/">Services</a>
-                    </li>
-                    <li class="b-nav__menu-item">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>beauty-resources/">Beauty Resources</a>
-                         <div class="b-nav__dropdown">
-                            <ul class="b-nav__col">
-                                <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>self-diagnosis/">Self Diagnosis</a></li>
-                                <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>beauty-at-any-age/">Beauty at Any Age</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="b-nav__menu-item">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>products/">Products</a>
-                         <div class="b-nav__dropdown">
-                            <ul class="b-nav__col">
-                                <?php $query = new WP_Query( array( 'post_type' => 'product', 'posts_per_page' => -1 ) ); ?>
-                                <?php if ( $query->have_posts() ) : ?>
-                                <?php while ( $query->have_posts() ) : $query->the_post();?>
-                                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-                                <?php endwhile; ?>
-                                <?php endif; ?>
-                                <?php wp_reset_query(); ?>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="b-nav__menu-item">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>specials/">Specials</a>
-                    </li>
-                    <li class="b-nav__menu-item">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>blog/">Blog</a>
-                    </li>
-                    <li class="b-nav__menu-item">
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>contact/">Contact</a>
-                    </li>
-                </ul> -->
+                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'b-nav__list') ); ?>
             </nav>
             <ul class="b-socials">
                 <li class="b-socials__item b-socials__item_facebook"><a href="">Facebook</a></li>
